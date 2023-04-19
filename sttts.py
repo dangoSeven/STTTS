eac4b1d4e6dfc8f65a73ea7c31ccbaee54511c66
dangoSeven <129814545+dangoSeven@users.noreply.github.com>
2023-04-18 20:17:49 -0600

    faster whisper!

--- sttts.py
+++ sttts.py
@@ -1,9 +1,6 @@
 import pyaudio
 import wave
 import numpy as np
-import sounddevice as sd
-import wavio
-import whisper
 import os
 import requests
 import json
@@ -12,8 +9,12 @@ import re
 import threading
 import queue
 import dotenv
+import soundfile as sf
+import sys
+import signal
 
 from collections import defaultdict
+from faster_whisper import WhisperModel
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -28,14 +29,16 @@ api_key = str(os.getenv('ELEVEN_LABS_API_KEY'))
 voice_id = str(os.getenv('VOICE_ID'))
 stability = os.getenv('STABILITY')
 similarity_boost = os.getenv('SIMILARITY_BOOST')
-
+vad = int(os.getenv('VAD'))
 
 played_files = defaultdict(lambda: False)
 
-model = whisper.load_model("small")
+# Load the faster-whisper model
+model = WhisperModel("small", device="cuda", compute_type="float16")
 
 file_queue = queue.Queue()
 
+exit_app = False
 
 def play_audio(filename):
     reduced_sample_rate = 44100  # Lower sample rate (default: 44100)
@@ -52,18 +55,21 @@ def play_audio(filename):
 
 def save_audio(filename, recording, samplerate=44100):
     data = np.array(recording * (2**15 - 1), dtype=np.int16)
-    wavio.write(filename, data, samplerate)
+    sf.write(filename, data, samplerate)
 
 def speech_to_text(filename):
-    data_file_path = os.path.join(os.path.dirname(__file__), filename)
-    result = model.transcribe(data_file_path)
-    text_result = result["text"]
-    print(result["text"])
+    data_file_path = os.path.join(os.path.dirname(sys.argv[0]), filename)
+    if vad < 1:
+        segments, _ = model.transcribe(data_file_path, beam_size=5)
+    else:
+        segments, _ = model.transcribe(data_file_path, vad_filter=True)
+    text_result = ' '.join([segment.text for segment in segments])
+    print(text_result)
 
-    if "exit app" in result["text"].lower():
+    if "exit app" in text_result.lower():
         quit()
-    if (re.sub(r'\W+', '', result["text"]).isalnum() and result["text"].isascii()):
-        text_to_speech(result["text"])
+    if (re.sub(r'\W+', '', text_result).isalnum() and text_result.isascii()):
+        text_to_speech(text_result)
 
 def text_to_speech(text):
     global api_key, voice_id, stability, similarity_boost
@@ -100,7 +106,7 @@ def is_silent(data, threshold=SILENCE_THRESHOLD):
 
 def process_audio_files():
     played_files = {}
-    while True:
+    while True and exit_app == False:
         if not file_queue.empty():
             filename = file_queue.get()
             played_files[filename] = False
@@ -111,17 +117,17 @@ fileCount = 0
 
 def record_audio():
     global fileCount
+    global exit_app
     p = pyaudio.PyAudio()
-
     stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
 
     print("Waiting for you to start talking...")
 
-    while True:
+    while True and exit_app == False:
         data = stream.read(CHUNK)
         data_np = np.frombuffer(data, dtype=np.int16)
         if not is_silent(data_np):
@@ -133,7 +139,7 @@ def record_audio():
     silent_chunks = 0
     recorded_chunks = 0
 
-    while True:
+    while True and exit_app == False:
         data = stream.read(CHUNK)
         data_np = np.frombuffer(data, dtype=np.int16)
         frames.append(data)
@@ -160,23 +166,35 @@ def record_audio():
             # If no played file is found, create a new one
             filename = WAVE_OUTPUT_FILENAME.format(fileCount)
 
-        wf = wave.open(filename, "wb")
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b"".join(frames))
-        wf.close()
+        with wave.open(filename, "wb") as wf:
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b"".join(frames))
 
         file_queue.put(filename)  # Add the file to the queue
         fileCount = fileCount + 1
     else:
         print("Recording too short, not saving.")
 
+def exit_handler(signal, frame):
+    global exit_app
+    print("\nExiting gracefully...")
+    exit_app = True
+    # Release any resources here
+    pygame.mixer.quit()
+    sys.exit(0)
 
 if __name__ == "__main__":
-        
+
+    # Register the exit handler for KeyboardInterrupt (Ctrl+C)
+    signal.signal(signal.SIGINT, exit_handler)
+
     processing_thread = threading.Thread(target=process_audio_files)
     processing_thread.start()
 
     while True:
-        record_audio()
+            try:
+                record_audio()
+            except KeyboardInterrupt:
+                exit_handler(None, None)
